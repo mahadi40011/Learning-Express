7b9ad26038ab97cb4c2537b48ec7872a6431d214
mahadi40011 <mahadi40011@gmail.com>
2026-06-03 22:44:38 +0600

    set types and use middleware. create get and post API

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,27 @@
-import express from "express"
-const app = express();
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+const app: Application = express();
 const port = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "This is root",
+    developer: "M. H. Mahbub",
+  });
+});
+
+app.post("/user", (req: Request, res: Response) => {
+  const user = req.body;
+  console.log(req.body);
+  res.status(201).json({
+    message: "User Created Successfully",
+    data: user,
+  });
 });
 
 app.listen(port, () => {
