9308e6a8f5b3270c9884de08a6cc5f11d3cbd8ad
mahadi40011 <mahadi40011@gmail.com>
2026-06-04 21:27:16 +0600

    Create a API for update operation

--- src/server.ts
+++ src/server.ts
@@ -115,6 +115,37 @@ app.get("/api/users/:id", async (req: Request, res: Response) => {
   }
 });
 
+app.put("/api/users/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { name, password, age, is_active } = req.body;
+    const result = await pool.query(
+      `UPDATE users SET name=$1, password=$2, age=$3, is_active=$4 WHERE id=$5 RETURNING *`,
+      [name, password, age, is_active, id],
+    );
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User not found!"
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: error,
+    });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
