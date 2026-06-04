b058a2e1f2d9db9397f9b5b95328384f79aa8596
mahadi40011 <mahadi40011@gmail.com>
2026-06-04 21:46:02 +0600

    Added COALESCE() function to the update query

--- src/server.ts
+++ src/server.ts
@@ -120,14 +120,21 @@ app.put("/api/users/:id", async (req: Request, res: Response) => {
     const { id } = req.params;
     const { name, password, age, is_active } = req.body;
     const result = await pool.query(
-      `UPDATE users SET name=$1, password=$2, age=$3, is_active=$4 WHERE id=$5 RETURNING *`,
+      `UPDATE users
+       SET
+       name=COALESCE($1, name),
+       password=COALESCE($2, password),
+       age=COALESCE($3, age),
+       is_active=COALESCE($4, is_active)
+       WHERE id=$5
+       RETURNING *`,
       [name, password, age, is_active, id],
     );
 
     if (result.rows.length === 0) {
       res.status(404).json({
         success: false,
-        message: "User not found!"
+        message: "User not found!",
       });
       return;
     }
