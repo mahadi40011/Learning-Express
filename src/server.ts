f59f8fccb12d45ec966fd4e8797fbb4d1ee6fafc
mahadi40011 <mahadi40011@gmail.com>
2026-06-04 19:47:23 +0600

    Create post api and send user data to db, added email validation

--- src/server.ts
+++ src/server.ts
@@ -21,8 +21,8 @@ const initDB = async () => {
     await pool.query(`
     CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY,
-    name VARCHAR(20),
-    email VARCHAR(20) NOT NULL,
+    name VARCHAR(30),
+    email VARCHAR(30) UNIQUE NOT NULL,
     password VARCHAR(20) NOT NULL,
     is_active BOOLEAN DEFAULT true,
     age INT,
@@ -35,22 +35,35 @@ const initDB = async () => {
     console.log(error);
   }
 };
-initDB()
+initDB();
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
-    message: "This is root",
+    message: "This is express js server",
     developer: "M. H. Mahbub",
   });
 });
 
-app.post("/user", (req: Request, res: Response) => {
-  const user = req.body;
-  console.log(req.body);
-  res.status(201).json({
-    message: "User Created Successfully",
-    data: user,
-  });
+app.post("/user", async (req: Request, res: Response) => {
+  const { name, email, password, age } = req.body;
+  try {
+    const result = await pool.query(
+      `
+    INSERT INTO users(name, email, password, age) VALUES($1, $2, $3, $4)   RETURNING *
+    `,
+      [name, email, password, age],
+    );
+
+    res.status(201).json({
+      message: "User Created Successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      message: error.message,
+      error: error,
+    });
+  }
 });
 
 app.listen(port, () => {
