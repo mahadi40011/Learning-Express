470004ca79fd29d683eaf629f362d3dde1200d3c
mahadi40011 <mahadi40011@gmail.com>
2026-06-04 20:10:46 +0600

    Create get api for Read all data

--- src/server.ts
+++ src/server.ts
@@ -66,6 +66,24 @@ app.post("/api/users", async (req: Request, res: Response) => {
   }
 });
 
+app.get("/api/users", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM users`);
+
+    res.status(200).json({
+      success: true,
+      message: "Users retrieved successfully",
+      data: result.rows,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: error,
+    });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
