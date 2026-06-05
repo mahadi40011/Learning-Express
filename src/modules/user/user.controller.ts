5f2e95431858be3a6c7c8e66e021364dffd16adc
mahadi40011 <mahadi40011@gmail.com>
2026-06-05 21:42:33 +0600

    Create user.controller.ts file for handle req-res

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,32 @@
+import type { Request, Response } from "express";
+import { pool } from "../../db";
+
+const createUser = async (req: Request, res: Response) => {
+  const { name, email, password, age } = req.body;
+  try {
+    const result = await pool.query(
+      `
+      INSERT INTO users(name, email, password, age) 
+      VALUES($1, $2, $3, $4)   
+      RETURNING *
+    `,
+      [name, email, password, age],
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "User Created Successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(409).json({
+      success: false,
+      message: error.message,
+      data: error,
+    });
+  }
+};
+
+export const userController = {
+  createUser,
+};
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,30 +1,8 @@
-import { Router, type Request, type Response } from "express";
-import { pool } from "../../db";
+import { Router } from "express";
+import { userController } from "./user.controller";
 
 const router = Router();
 
-router.post("/", async (req: Request, res: Response) => {
-  const { name, email, password, age } = req.body;
-  try {
-    const result = await pool.query(
-      `
-    INSERT INTO users(name, email, password, age) VALUES($1, $2, $3, $4)   RETURNING *
-    `,
-      [name, email, password, age],
-    );
-
-    res.status(201).json({
-      success: true,
-      message: "User Created Successfully",
-      data: result.rows[0],
-    });
-  } catch (error: any) {
-    res.status(409).json({
-      success: false,
-      message: error.message,
-      data: error,
-    });
-  }
-});
+router.post("/", userController.createUser);
 
 export const userRouter = router;
