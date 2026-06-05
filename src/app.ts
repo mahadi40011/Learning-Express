3b13bec1dc38f57468ac41f62b7d4e05e9ae3a2a
mahadi40011 <mahadi40011@gmail.com>
2026-06-06 00:16:57 +0600

    create a post api end point, create function for req and res handeling, write a database logic for insert a profile data

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import express, {
   type Response,
 } from "express";
 import { userRouter } from "./modules/user/user.route";
+import { profileRouter } from "./modules/profile/profile.route";
 const app: Application = express();
 
 app.use(express.json());
@@ -19,5 +20,6 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/api/users", userRouter);
+app.use("/api/profile", profileRouter);
 
 export default app;
