9bda28cbe1a53ee6b77f70ca7191b5dfd44f310f
mahadi40011 <mahadi40011@gmail.com>
2026-06-05 23:17:01 +0600

    redecorate the users table query

--- src/db/index.ts
+++ src/db/index.ts
@@ -9,16 +9,17 @@ export const initDB = async () => {
   try {
     await pool.query(`
     CREATE TABLE IF NOT EXISTS users(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(30),
-    email VARCHAR(30) UNIQUE NOT NULL,
-    password VARCHAR(20) NOT NULL,
-    is_active BOOLEAN DEFAULT true,
-    age INT,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
-    )
-    `);
+      id SERIAL PRIMARY KEY,
+
+      name VARCHAR(30),
+      email VARCHAR(30) UNIQUE NOT NULL,
+      password VARCHAR(20) NOT NULL,
+      is_active BOOLEAN DEFAULT true,
+      age INT,
+
+      created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+      updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+    )`);
 
     await pool.query(`
     CREATE TABLE IF NOT EXISTS profiles(
