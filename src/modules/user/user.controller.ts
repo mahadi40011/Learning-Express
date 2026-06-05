a365ec2698c50bd189346d220c285adac22ea845
mahadi40011 <mahadi40011@gmail.com>
2026-06-05 21:52:45 +0600

    create user.service.ts file for handle database querys

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,17 +1,10 @@
 import type { Request, Response } from "express";
 import { pool } from "../../db";
+import { userService } from "./user.service";
 
 const createUser = async (req: Request, res: Response) => {
-  const { name, email, password, age } = req.body;
   try {
-    const result = await pool.query(
-      `
-      INSERT INTO users(name, email, password, age) 
-      VALUES($1, $2, $3, $4)   
-      RETURNING *
-    `,
-      [name, email, password, age],
-    );
+    const result = await userService.createUserIntoDB(req.body);
 
     res.status(201).json({
       success: true,
