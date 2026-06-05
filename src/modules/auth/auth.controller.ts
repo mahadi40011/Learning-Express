af15bd13d7c440fafe9abc1033b596a6c0f7cffe
mahadi40011 <mahadi40011@gmail.com>
2026-06-06 02:23:17 +0600

    write a database query for find a user and match the password

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,11 +4,11 @@ import { authService } from "./auth.service";
 const loginUser = async (req: Request, res: Response) => {
   try {
     const result = await authService.loginUserIntoDB(req.body);
-    // res.status(201).json({
-    //   success: true,
-    //   message: "Profile created successfully",
-    //   data: result.rows[0],
-    // });
+    res.status(201).json({
+      success: true,
+      message: "Login successful",
+      data: result
+    });
   } catch (error: any) {
     res.status(500).json({
       success: false,
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,28 @@
-const loginUserIntoDB = async (payload: { name: string; email: string }) => {};
+import bcrypt from "bcryptjs";
+import { pool } from "../../db";
+
+const loginUserIntoDB = async (payload: {
+  email: string;
+  password: string;
+}) => {
+  // check the user exist --> done
+  // compare the password --> done
+  // generate token
+  const { password, email } = payload;
+  const userData = await pool.query(`SELECT * FROM users WHERE email=$1`, [
+    email,
+  ]);
+  if (userData.rows.length === 0) {
+    throw new Error("Invalid Credential!")
+  }
+
+  const user = userData.rows[0]
+  const matchPassword = bcrypt.compare(password, user?.password)
+  if (!matchPassword) {
+    throw new Error("Invalid Password");
+  }
+
+};
 
 export const authService = {
   loginUserIntoDB,
