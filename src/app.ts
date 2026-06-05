5aeaf53094b23c5663d02bfb41d7af0fd7065737
mahadi40011 <mahadi40011@gmail.com>
2026-06-05 19:55:12 +0600

    Create mrthod code moved to user.route.ts file

--- src/app.ts
+++ src/app.ts
@@ -21,29 +21,7 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use("/api/users", userRouter);
 
-app.post("/api/users", async (req: Request, res: Response) => {
-  const { name, email, password, age } = req.body;
-  try {
-    const result = await pool.query(
-      `
-    INSERT INTO users(name, email, password, age) VALUES($1, $2, $3, $4)   RETURNING *
-    `,
-      [name, email, password, age],
-    );
 
-    res.status(201).json({
-      success: true,
-      message: "User Created Successfully",
-      data: result.rows[0],
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      success: false,
-      message: error.message,
-      data: error,
-    });
-  }
-});
 
 app.get("/api/users", async (req: Request, res: Response) => {
   try {
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,5 +1,30 @@
-import { Router } from "express";
+import { Router, type Request, type Response } from "express";
+import { pool } from "../../db";
 
 const router = Router();
+
+router.post("/", async (req: Request, res: Response) => {
+  const { name, email, password, age } = req.body;
+  try {
+    const result = await pool.query(
+      `
+    INSERT INTO users(name, email, password, age) VALUES($1, $2, $3, $4)   RETURNING *
+    `,
+      [name, email, password, age],
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "User Created Successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(409).json({
+      success: false,
+      message: error.message,
+      data: error,
+    });
+  }
+});
 
 export const userRouter = router;
