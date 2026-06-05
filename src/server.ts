b546f5b067d556146b5698a2e8e3eccf43caa2b6
mahadi40011 <mahadi40011@gmail.com>
2026-06-05 19:31:15 +0600

    Create db folder for stor db model

--- src/server.ts
+++ src/server.ts
@@ -12,29 +12,7 @@ app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
 
-const pool = new Pool({
-  connectionString: config.connection_string,
-});
 
-const initDB = async () => {
-  try {
-    await pool.query(`
-    CREATE TABLE IF NOT EXISTS users(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(30),
-    email VARCHAR(30) UNIQUE NOT NULL,
-    password VARCHAR(20) NOT NULL,
-    is_active BOOLEAN DEFAULT true,
-    age INT,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
-    )
-    `);
-    console.log("Database Connected Successfully");
-  } catch (error) {
-    console.log(error);
-  }
-};
 initDB();
 
 app.get("/", (req: Request, res: Response) => {
