805975ebd0784621b0539a8d91df743216e316b3
mahadi40011 <mahadi40011@gmail.com>
2026-06-06 18:01:06 +0600

    move the logger middleware to the logger.ts file

--- src/app.ts
+++ src/app.ts
@@ -7,20 +7,14 @@ import { userRouter } from "./modules/user/user.route";
 import { profileRouter } from "./modules/profile/profile.route";
 import { authRouter } from "./modules/auth/auth.route";
 import fs from "fs";
+import logger from "./middleware/logger";
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
-
-app.use((req, res, next) => {
-  const log = `\nMethod --> ${req.method}, URL --> ${req.url}, Time --> ${Date.now()}\n`;
-  fs.appendFile("logger.txt", log, (error) => {
-    console.log(error);
-  });
-  next();
-});
+app.use(logger);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
