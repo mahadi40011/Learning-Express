d6517714070dd32ad3b3eddb26c74d21004070ab
mahadi40011 <mahadi40011@gmail.com>
2026-06-06 19:41:45 +0600

    checking  the user exist and user active or not

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -21,8 +21,22 @@ const authMiddleware = () => {
     const userData = await pool.query(`SELECT * FROM users WHERE email=$1`, [
       decoded.email,
     ]);
+    
+    if (userData.rows.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found!",
+      });
+    }
+
     const user = userData.rows[0];
-    console.log(user);
+
+    if (!user.is_active) {
+      return res.status(403).json({
+        success: false,
+        message: "Forbidden!!",
+      });
+    }
 
     next();
   };
