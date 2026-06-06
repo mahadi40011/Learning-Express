25310210477f76695a42df41171e80df52129408
mahadi40011 <mahadi40011@gmail.com>
2026-06-06 19:53:44 +0600

    Added express namespace file. auth middleware code rewrite into try-catch block

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -5,40 +5,44 @@ import { pool } from "../db";
 
 const authMiddleware = () => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization;
-    if (!token) {
-      return res.status(401).json({
-        success: false,
-        message: "Unauthorized Access",
-      });
+    try {
+      const token = req.headers.authorization;
+      if (!token) {
+        return res.status(401).json({
+          success: false,
+          message: "Unauthorized Access",
+        });
+      }
+
+      const decoded = jwt.verify(
+        token,
+        config.jwt_secret as string,
+      ) as JwtPayload;
+
+      const userData = await pool.query(`SELECT * FROM users WHERE email=$1`, [
+        decoded.email,
+      ]);
+      const user = userData.rows[0];
+
+      if (userData.rows.length === 0) {
+        return res.status(404).json({
+          success: false,
+          message: "User not found!",
+        });
+      }
+
+      if (!user.is_active) {
+        return res.status(403).json({
+          success: false,
+          message: "Forbidden!!",
+        });
+      }
+
+      req.user = decoded;
+      next();
+    } catch (error) {
+      next(error);
     }
-
-    const decoded = jwt.verify(
-      token,
-      config.jwt_secret as string,
-    ) as JwtPayload;
-
-    const userData = await pool.query(`SELECT * FROM users WHERE email=$1`, [
-      decoded.email,
-    ]);
-    
-    if (userData.rows.length === 0) {
-      return res.status(404).json({
-        success: false,
-        message: "User not found!",
-      });
-    }
-
-    const user = userData.rows[0];
-
-    if (!user.is_active) {
-      return res.status(403).json({
-        success: false,
-        message: "Forbidden!!",
-      });
-    }
-
-    next();
   };
 };
 
